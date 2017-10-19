c429d0f880db0d518e961e2c2d8db7d306830959
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-10-19 15:30:03 -0700

    Add well constraints

--- src/landmatch.jl
+++ src/landmatch.jl
@@ -18,52 +18,43 @@ function landmatch!(landscapes::Vector{<:Matrix}, wells::AbstractVector{Well};
   nx, ny = size(landscapes[1])
   nlandscapes = length(landscapes)
 
-  # JuMP model
-  m = Model(solver=ECOSSolver(verbose=false))
-
-  # input landscape (data matrix)
-  E = Array{Float64}(nx, ny)
-
-  # optimization variables
-  @variable(m, L[1:nx,1:ny])
-  @variable(m, t[1:nx,1:ny])
-
-  # convex problem in epigraph form
-  @objective(m, Min, sum(t))
-  @constraint(m, [i=1:nx,j=1:ny], t[i,j] ≥ 0)
-  @constraint(m, upperbound[i=1:nx,j=1:ny], L[i,j] - E[i,j] ≤ t[i,j])
-  @constraint(m, lowerbound[i=1:nx,j=1:ny], E[i,j] - L[i,j] ≤ t[i,j])
-
-  # smoothness constraints on 3rd derivative
-  @constraint(m, [i=3:nx-1,j=1:ny], L[i+1,j] - 3L[i,j] + 3L[i-1,j] - L[i-2,j] ≤  tol)
-  @constraint(m, [i=3:nx-1,j=1:ny], L[i+1,j] - 3L[i,j] + 3L[i-1,j] - L[i-2,j] ≥ -tol)
-  @constraint(m, [i=1:nx,j=3:ny-1], L[i,j+1] - 3L[i,j] + 3L[i,j-1] - L[i,j-2] ≤  tol)
-  @constraint(m, [i=1:nx,j=3:ny-1], L[i,j+1] - 3L[i,j] + 3L[i,j-1] - L[i,j-2] ≥ -tol)
-
   # show progress and estimated time duration
   showprogress && (progress = Progress(nlandscapes, color=:black))
 
   # loop over landscapes
-  for i=1:nlandscapes
-    # initialize input landscape
-    E = landscapes[i]
+  for l=1:nlandscapes
+    # input landscape
+    E = landscapes[l]
 
-    # update RHS of LP problem
-    JuMP.setRHS.(upperbound,  E)
-    JuMP.setRHS.(lowerbound, -E)
+    # JuMP model
+    m = Model(solver=ECOSSolver(verbose=false))
 
-    # update well constraints
-    # TODO
+    # optimization variables
+    @variable(m, L[1:nx,1:ny])
+    @variable(m, t[1:nx,1:ny])
+
+    # convex problem in epigraph form
+    @objective(m, Min, sum(t))
+    @constraint(m, [i=1:nx,j=1:ny], t[i,j] ≥ 0)
+    @constraint(m, [i=1:nx,j=1:ny], L[i,j] - E[i,j] ≤ t[i,j])
+    @constraint(m, [i=1:nx,j=1:ny], E[i,j] - L[i,j] ≤ t[i,j])
+
+    # smoothness constraints on 3rd derivative
+    @constraint(m, [i=3:nx-1,j=1:ny], L[i+1,j] - 3L[i,j] + 3L[i-1,j] - L[i-2,j] ≤  tol)
+    @constraint(m, [i=3:nx-1,j=1:ny], L[i+1,j] - 3L[i,j] + 3L[i-1,j] - L[i-2,j] ≥ -tol)
+    @constraint(m, [i=1:nx,j=3:ny-1], L[i,j+1] - 3L[i,j] + 3L[i,j-1] - L[i,j-2] ≤  tol)
+    @constraint(m, [i=1:nx,j=3:ny-1], L[i,j+1] - 3L[i,j] + 3L[i,j-1] - L[i,j-2] ≥ -tol)
+
+    # well constraints
+    for well in wells
+      @constraint(m, L[well.coords...] == well.values[l])
+    end
 
     # solve
     status = solve(m)
 
-    if status == :Optimal
-      # update landscape
-      landscapes[i] = getvalue(L)
-    else
-      warn("Failed to find optimal value")
-    end
+    # update landscape
+    landscapes[l] = getvalue(L)
 
     # update progress bar
     showprogress && next!(progress)
