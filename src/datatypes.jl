8437eeaafe14785194d95824a1ac7d92b30d7cb5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-01-25 13:17:54 -0200

    Remove outdated files

--- src/datatypes.jl
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------
-# Copyright (c) 2017, Júlio Hoffimann Mendes <juliohm@stanford.edu>
-# Licensed under the ISC License. See LICENCE in the project root.
-# ------------------------------------------------------------------
-
-struct Well
-  coords::Tuple{Int,Int}
-  values::Vector{<:Real}
-end
